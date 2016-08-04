8ff13e0af5c09020f8a27c1aeb82d4890e426d83
Pete M McCarthy <pete.m.mccarthy@gmail.com>
2016-08-04 21:32:33 +0100

    products and tests aded

--- app/components/product-list/component.js
+++ app/components/product-list/component.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['product'],
+  products: [{
+    name: 'cat',
+    price: 5
+  }, {
+    name: 'dog',
+    price: 10
+  }]
 });
