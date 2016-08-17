e41639133ebd8beae8f788f879e738303f8a2853
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-17 21:06:36 +0100

    refactored layout, nav and product components removed as coming from api

--- app/home/route.js
+++ app/home/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  actions: {
+    showAnimals () {
+      this.transitionTo('home.animals');
+    },
+    showContact () {
+      this.transitionTo('home.contact');
+    }
+  }
+
 });
