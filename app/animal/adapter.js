a9508bec081c690fbec0879fa2fc30946be339de
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-17 15:51:44 +0100

    adapter cleaned up

--- app/animal/adapter.js
+++ app/animal/adapter.js
@@ -2,9 +2,9 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   host: 'http://localhost:3000',
-  // host: 'http://jsonplaceholder.typicode.com',
-  // namespace: 'Users/Peter/apps/web_shop',
+
   pathForType() {
     return 'animals';
   }
+
 });
