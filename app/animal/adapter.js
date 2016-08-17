ce8599e7262a264943a0531e3d674462f2758e0e
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-17 14:37:31 +0100

    adapter, model and route added

--- app/animal/adapter.js
+++ app/animal/adapter.js
@@ -1,4 +1,10 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:3000',
+  // host: 'http://jsonplaceholder.typicode.com',
+  // namespace: 'Users/Peter/apps/web_shop',
+  pathForType() {
+    return 'animals';
+  }
 });
