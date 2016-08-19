766cf42042e99237d5614936a8512d37bc1629bd
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-19 10:52:30 +0100

    menu now links not actions and fixed to top

--- app/home/route.js
+++ app/home/route.js
@@ -1,14 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
-  actions: {
-    showAnimals () {
-      this.transitionTo('home.animals');
-    },
-    showContact () {
-      this.transitionTo('home.contact');
-    }
-  }
-
 });
