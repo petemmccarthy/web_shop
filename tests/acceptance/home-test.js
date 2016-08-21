ff9432b748e1ccb286e4b357ecfe8c481c1849db
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-21 19:51:08 +0100

    links tests

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -3,7 +3,7 @@ import moduleForAcceptance from 'web-shop/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | home');
 
-test('visiting /home', function(assert) {
+test('home page', function(assert) {
   visit('/');
 
   andThen(function() {
@@ -11,5 +11,30 @@ test('visiting /home', function(assert) {
     assert.equal(find('.title').length, 1);
     assert.equal(find('#animalsLink').length, 1);
     assert.equal(find('#contactLink').length, 1);
+
+  });
+});
+
+test('visiting contact link', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('#animalsLink a');
+  });
+
+  andThen(function() {
+    assert.equal(currentURL(), '/animals');
+  });
+});
+
+test('visiting contact link', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('#contactLink a');
+  });
+
+  andThen(function() {
+    assert.equal(currentURL(), '/contact');
   });
 });
