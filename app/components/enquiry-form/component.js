4723d51efd27879d9138b127f05ae9c7a94cf902
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-23 17:24:31 +0100

    title selectbox added

--- app/components/enquiry-form/component.js
+++ app/components/enquiry-form/component.js
@@ -1,4 +1,52 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['enquiry-form'],
+  titles: [
+    {
+      label: 'Mr',
+      value: 'Mr'
+    },
+    {
+      label: 'Mrs',
+      value: 'Mrs'
+    },
+    {
+      label: 'Ms',
+      value: 'Ms'
+    },
+    {
+      label: 'Miss',
+      value: 'Miss'
+    },
+    {
+      label: 'Dr',
+      value: 'Dr'
+    },
+    {
+      label: 'Sir',
+      value: 'Sir'
+    },
+    {
+      label: 'Reverend',
+      value: 'Reverend'
+    },
+    {
+      label: 'Dame',
+      value: 'Dame'
+    },
+    {
+      label: 'Lady',
+      value: 'Lady'
+    },
+    {
+      label: 'Lord',
+      value: 'Lord'
+    },
+    {
+      label: 'Professor',
+      value: 'Professor'
+    }
+  ]
+
 });
