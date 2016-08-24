012c92002ed6ca7fbd0cf97d50b549168c5eeb3a
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-08-24 20:03:41 +0100

    enquire route added

---
+++ tests/unit/home/animals/enquire/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:home/animals/enquire', 'Unit | Route | home/animals/enquire', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
