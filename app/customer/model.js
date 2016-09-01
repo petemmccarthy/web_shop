4d8a7e0be31042df2b8e4de40f0df64c02838749
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-09-01 16:27:05 +0100

    selectedAnimal added to customer model

--- app/customer/model.js
+++ app/customer/model.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  selectedAnimal: DS.attr('string'),
   title: DS.attr('string'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
