078121bce2954bde57e980e5d7b3019e43ec2865
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-19 15:52:42 +0100

    enuiry link tests added

--- tests/acceptance/animals-test.js
+++ tests/acceptance/animals-test.js
@@ -9,5 +9,18 @@ test('visiting /animals', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/animals');
     assert.equal(find('#animals').length, 1);
+    assert.equal(find('#enquiryLink').length, 1);
+  });
+});
+
+test('selecting animal link', function(assert) {
+  visit('/animals');
+
+  andThen(function() {
+    click('#enquiryLink a');
+  });
+
+  andThen(function() {
+    assert.equal(currentURL(), '/animals/enquire');
   });
 });
