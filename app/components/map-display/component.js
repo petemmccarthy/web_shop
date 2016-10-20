b7f87b31464fadd4386894de5ffe35b6ad5b487b
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-20 14:28:25 +0100

    map now rendering first time and from component

--- app/components/map-display/component.js
+++ app/components/map-display/component.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  didInsertElement() {
+    this._super(...arguments);
+    var mapDiv = document.getElementById('map');
+    console.log(mapDiv);
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 51.5092928, lng: -0.10},
+        zoom: 14
+    });
+  }
+
 });
