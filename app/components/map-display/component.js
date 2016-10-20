f62b15a611db9596a431d7f9ac1c0b33866d53ce
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-20 14:38:29 +0100

    code clean up

--- app/components/map-display/component.js
+++ app/components/map-display/component.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 
+const google = window.google;
+
 export default Ember.Component.extend({
 
   didInsertElement() {
     this._super(...arguments);
-    var mapDiv = document.getElementById('map');
-    console.log(mapDiv);
-    var map = new google.maps.Map(mapDiv, {
-        center: {lat: 51.5092928, lng: -0.10},
-        zoom: 14
+    let mapDiv = document.getElementById('map');
+    new google.maps.Map(mapDiv, {
+      center: {lat: 51.5092928, lng: -0.10},
+      zoom: 14
     });
   }
 
